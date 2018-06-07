af3feaf267b1f8bcb3ad5bd05e510a7fb5fc9982
Lev Erminson <erminson@gmail.com>
2018-06-07 19:07:39 +0300

    Followed the hero around the map with camera

---
+++ Assets/Scripts/CameraBounds.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraBounds : MonoBehaviour {
+    public float minVisibleX;
+    public float maxVisibleX;
+
+    private float minValue;
+    private float maxValue;
+
+    public float cameraHalfWidth;
+
+    private Camera activeCamera;
+
+    public Transform cameraRoot;
+
+    public Transform leftBounds;
+    public Transform rightBounds;
+
+    void Start() {
+        activeCamera = Camera.main;
+
+        float delta = activeCamera.ScreenToWorldPoint(new Vector3(0, 0, 0)).x -
+                                  activeCamera.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x;
+        cameraHalfWidth = Mathf.Abs(delta) * 0.5f;
+
+        minValue = minVisibleX + cameraHalfWidth;
+        maxValue = maxVisibleX - cameraHalfWidth;
+
+        Vector3 position;
+        position = leftBounds.transform.localPosition;
+        position.x = transform.localPosition.x - cameraHalfWidth;
+        leftBounds.transform.localPosition = position;
+
+        position = rightBounds.transform.localPosition;
+        position.x = transform.localPosition.x + cameraHalfWidth;
+        rightBounds.transform.localPosition = position;
+    }
+
+    public void SetPosition(float x) {
+        Vector3 trans = cameraRoot.position;
+        trans.x = Mathf.Clamp(x, minValue, maxValue);
+        cameraRoot.position = trans;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public Hero actor;
+    public bool cameraFollows = true;
+    public CameraBounds cameraBounds;
+
+    void Start() {
+        cameraBounds.SetPosition(cameraBounds.minVisibleX);
+    }
+
+    void Update() {
+        if (cameraFollows) {
+            cameraBounds.SetPosition(actor.transform.position.x);
+        }
+    }
+}
