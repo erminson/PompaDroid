7fa66506a64457b949b0b61e5441aee750ea2b58
Lev Erminson <erminson@gmail.com>
2018-06-08 13:02:39 +0300

    Implement run of hero

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -9,6 +9,14 @@ public class Hero : MonoBehaviour {
 
     public float speed = 2;
     public float walkSpeed = 2;
+    public float runSpeed = 5;
+
+    bool isRunning;
+    bool isMoving;
+    float lastWalk;
+    public bool canRun = true;
+    float tapAgainToRunTime = 0.2f;
+    Vector3 lastWalkVector;
 
     Vector3 currentDir;
     bool isFacingLeft;
@@ -24,19 +32,43 @@ public class Hero : MonoBehaviour {
 
         if (v == 0 && h == 0) {
             Stop();
-        } else if (v != 0 || h != 0){
-            Walk();
+            isMoving = false;
+        } else if (!isMoving && (v != 0 || h != 0)) {
+            isMoving = true;
+            float dotProduct = Vector3.Dot(currentDir, lastWalkVector);
+
+            if (canRun && Time.time < lastWalk + tapAgainToRunTime && dotProduct > 0) {
+                Run();
+            } else {
+                Walk();
+            }
+
+            if (h != 0) {
+                lastWalkVector = currentDir;
+                lastWalk = Time.time;
+            }
         }
     }
 
     void Walk() {
         speed = walkSpeed;
+        isRunning = false;
         baseAnim.SetFloat("Speed", speed);
+        baseAnim.SetBool("isRunning", isRunning);
     }
 
     void Stop() {
         speed = 0;
+        isRunning = false;
         baseAnim.SetFloat("Speed", speed);
+        baseAnim.SetBool("isRunning", isRunning);
+    }
+
+    void Run() {
+        speed = runSpeed;
+        isRunning = true;
+        baseAnim.SetFloat("Speed", speed);
+        baseAnim.SetBool("isRunning", isRunning);
     }
 
     void FixedUpdate() {
