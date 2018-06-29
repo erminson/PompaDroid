29ec0e3bc6c85ce3ccf67a6096af66b0777740ed
Lev Erminson <erminson@gmail.com>
2018-06-29 12:12:10 +0300

    Added on-screen controls - attack, jump, movement buttons

---
+++ Assets/Scripts/ActionDPad.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ActionDPad : UIBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public enum ActionPadDirection
+    {
+        Up = 1,
+        UpRight = 2,
+        Right = 3,
+        DownRight,
+        Down,
+        DownLeft,
+        Left,
+        UpLeft,
+        None = 999
+    }
+
+    [SerializeField]
+    float radius = 1;
+
+    [HideInInspector]
+    bool isHeld;
+
+    [SerializeField]
+    Sprite[] directionalSprites;
+
+    [Serializable]
+    public class JoystickMoveEvent: UnityEvent<ActionPadDirection>
+    {
+    }
+
+    public JoystickMoveEvent OnValueChange;
+
+    private ActionPadDirection UpdateTouchSprite(Vector2 direction)
+    {
+        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+
+        if (angle < 0)
+        {
+            angle += 360;
+        }
+
+        ActionPadDirection currentPadDirection = ActionPadDirection.None;
+        if (angle <= 22.5f || angle > 337.5f)
+        {
+            currentPadDirection = ActionPadDirection.Up;
+        }
+        else if (angle > 22.5 && angle <= 67.5)
+        {
+            currentPadDirection = ActionPadDirection.UpRight;
+        }
+        else if (angle > 67.5 && angle <= 112.5)
+        {
+            currentPadDirection = ActionPadDirection.Right;
+        }
+        else if (angle > 112.5 && angle <= 157.5)
+        {
+            currentPadDirection = ActionPadDirection.DownRight;
+        }
+        else if (angle > 157.5 && angle <= 202.5)
+        {
+            currentPadDirection = ActionPadDirection.Down;
+        }
+        else if (angle > 202.5 && angle <= 247.5)
+        {
+            currentPadDirection = ActionPadDirection.DownLeft;
+        }
+        else if (angle > 247.5 && angle <= 292.5)
+        {
+            currentPadDirection = ActionPadDirection.Left;
+        }
+        else if (angle > 292.5 && angle <= 337.5)
+        {
+            currentPadDirection = ActionPadDirection.UpLeft;
+        }
+
+        int index = 0;
+        if (currentPadDirection != ActionPadDirection.None)
+        {
+            index = (int)currentPadDirection;
+        }
+
+        GetComponent<Image>().sprite = directionalSprites[index];
+
+        return currentPadDirection;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (!IsActive())
+        {
+            return;
+        }
+
+        RectTransform thisRect = transform as RectTransform;
+        Vector2 touchDir;
+        bool didConvert = RectTransformUtility.ScreenPointToLocalPointInRectangle(thisRect, eventData.position, eventData.enterEventCamera, out touchDir);
+
+        if (touchDir.sqrMagnitude > radius *radius)
+        {
+            touchDir.Normalize();
+            isHeld = true;
+            ActionPadDirection currentDirection = UpdateTouchSprite(touchDir);
+            OnValueChange.Invoke(currentDirection);
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        OnValueChange.Invoke(ActionPadDirection.None);
+        GetComponent<Image>().sprite = directionalSprites[0];
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (isHeld)
+        {
+            RectTransform thisRect = transform as RectTransform;
+            Vector2 touchDir;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(thisRect, eventData.position, eventData.enterEventCamera, out touchDir);
+            ActionPadDirection currentDirection = UpdateTouchSprite(touchDir);
+            OnValueChange.Invoke(currentDirection);
+        }
+    }
+
+    // IPointerDownHandler
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        RectTransform thisRect = transform as RectTransform;
+        Vector2 touchDir;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(thisRect, eventData.position, eventData.enterEventCamera, out touchDir);
+        touchDir.Normalize();
+
+        ActionPadDirection currentDirection = UpdateTouchSprite(touchDir);
+        OnValueChange.Invoke(currentDirection);
+    }
+
+    // IPointerUpHandler
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        OnValueChange.Invoke(ActionPadDirection.None);
+        GetComponent<Image>().sprite = directionalSprites[0];
+    }
+}
--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputHandler : MonoBehaviour
 {
@@ -10,6 +11,9 @@ public class InputHandler : MonoBehaviour
     float lastJustTime;
     bool isJumping;
     public float maxJumpDuration = 0.2f;
+
+    bool didAttack;
+    public bool useUI = true;
 
     public float GetVerticalAxis()
     {
@@ -33,22 +37,95 @@ public class InputHandler : MonoBehaviour
 
     private void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
-        attack = Input.GetButtonDown("Attack");
+        if (useUI)
+        {
+            if (didAttack)
+            {
+                didAttack = attack = false;
+            }
+            else if (attack)
+            {
+                didAttack = true;
+            }
+        } 
+        else
+        {
+            horizontal = Input.GetAxisRaw("Horizontal");
+            vertical = Input.GetAxisRaw("Vertical");
+            attack = Input.GetButtonDown("Attack");
 
-        if (!jump && !isJumping && Input.GetButton("Jump")) {
-            jump = true;
-            lastJustTime = Time.time;
-            isJumping = true;
-        } else if (!Input.GetButton("Jump")) {
-            jump = false;
-            isJumping = false;
-        }
+            if (!jump && !isJumping && Input.GetButton("Jump"))
+            {
+                jump = true;
+                lastJustTime = Time.time;
+                isJumping = true;
+            }
+            else if (!Input.GetButton("Jump"))
+            {
+                jump = false;
+                isJumping = false;
+            }
 
-        if (jump && Time.time > lastJustTime + maxJumpDuration) {
-            jump = false;
+            if (jump && Time.time > lastJustTime + maxJumpDuration)
+            {
+                jump = false;
+            } 
         }
     }
 
+    public void DidPressAttack(BaseEventData data)
+    {
+        attack = true;
+        didAttack = false;
+    }
+
+    public void DidPressJump(BaseEventData data)
+    {
+        if (!jump)
+        {
+            jump = true;
+            lastJustTime = Time.time;
+        }
+    }
+
+    public void DidReleaseJump(BaseEventData data)
+    {
+        jump = false;
+    }
+
+    public Vector2 VectorForPadDirection(ActionDPad.ActionPadDirection padDirection)
+    {
+        float maxX = 1.0f;
+        float maxY = 1.1f;
+        switch (padDirection)
+        {
+            case ActionDPad.ActionPadDirection.None:
+                return Vector2.zero;
+            case ActionDPad.ActionPadDirection.Up:
+                return new Vector2(0, maxY);
+            case ActionDPad.ActionPadDirection.UpRight:
+                return new Vector2(maxX, maxY);
+            case ActionDPad.ActionPadDirection.Right:
+                return new Vector2(maxX, 0);
+            case ActionDPad.ActionPadDirection.DownRight:
+                return new Vector2(maxX, -maxY);
+            case ActionDPad.ActionPadDirection.Down:
+                return new Vector2(0, -maxY);
+            case ActionDPad.ActionPadDirection.DownLeft:
+                return new Vector2(-maxX, -maxY);
+            case ActionDPad.ActionPadDirection.Left:
+                return new Vector2(-maxX, 0);
+            case ActionDPad.ActionPadDirection.UpLeft:
+                return new Vector2(-maxX, maxY);
+            default:
+                return Vector2.zero;
+        }
+    }
+
+    public void OnActionPadChangeDirection(ActionDPad.ActionPadDirection direction)
+    {
+        Vector2 directionVector = VectorForPadDirection(direction);
+        horizontal = directionVector.x;
+        vertical = directionVector.y;
+    }
 }
